11ad4ffe682d36134aa0cd559f63e7b88a5d13ba
Robert He <hnrobert@qq.com>
2026-04-17 11:00:25 +0800

    feat: declare CSS module in TypeScript definitions

--- webview/src/env.d.ts
+++ webview/src/env.d.ts
@@ -1,3 +1,8 @@
+declare module '*.css' {
+  const content: string;
+  export default content;
+}
+
 declare module '*.vue' {
   import type { DefineComponent } from 'vue';
   const component: DefineComponent<{}, {}, any>;
